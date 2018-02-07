cc7608bad847afd075b51c82eeb6d3168c6842ee
Matt Barnett <Madison@Madisons-MacBook-Pro.local>
2018-02-06 23:22:22 -0500

    note.js going strong

--- models/Note.js
+++ models/Note.js
@@ -8,4 +8,14 @@ var mongoose = require("mongoose");
 // Taking the schema constructor and saving a ref to it to keep things copacetic again
 var Schema = mongoose.Schema;
 
-
+// New NoteSchema object again
+var NoteSchema = new Schema({
+    title: String,
+    body: String
+  });
+  
+  // Creating model with mongoose model model method jjust like in Article.js
+  var Note = mongoose.model("Note", NoteSchema);
+  
+  // Exporting Note model
+  module.exports = Note;
