735a4aaf532bc794871a2531a0f3733483ebca5d
Matt Barnett <Madison@Madisons-MacBook-Pro.local>
2018-02-06 22:52:42 -0500

    probably should have done this a while ago

---
+++ models/index.js
@@ -0,0 +1,7 @@
+// Exporting object for a models
+
+module.exports = {
+    Article: require("./Article"),
+    Note: require("./Note")
+  };
+  
---
+++ server.js
@@ -0,0 +1,42 @@
+// Dependencies required
+var express = require("express");
+var handlebars = require("express-handlebars");
+var mongoose = require("mongoose"); 
+var bodyParser = require("body-parser");
+var request = require("request");
+var logger = require("morgan");
+
+// These little thugs are for scraping things that are not mine
+var cheerio = require("cheerio");
+var axios = require("axios");
+
+// My port, requiring models, and initializing express
+
+var PORT = 3000;
+var app = express();
+var db = require("./models");
+
+// Mongo db connection and mongoose for .then
+mongoose.Promise = Promise;
+mongoose.connect("mongodb://localhost/scrapingWithMongoose", {
+    useMongoClient: true
+});
+
+// Making the public folder static
+app.use(express.static("public"));
+
+// To handle submitting forms
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// To log requests
+app.use(logger("dev"));
+
+// Get route to scrape Space.com
+app.get("/scrape", function(req, res) {
+    // Grabbing entire body of the html (wondering if this will work how i expect)
+    axios.get(https://www.space.com/tech-robots).then(function(response) {
+        // loading to cheerio and making it a shorthand selector
+        var $ = cheerio.load(response.data);
+    })
+})
+
